35450fe31588b1c8b2bd36505e93a6505247222a
Adyo Prawira <adyoprawira@gmail.com>
2026-05-24 15:19:41 +1000

    Fix deployment 5

--- Project/urls.py
+++ Project/urls.py
@@ -1,20 +1,15 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, re_path
 from django.views.static import serve
 
 frontend_root = settings.BASE_DIR / 'Code' / 'frontend'
 
 urlpatterns = [
-    path('', serve, {'path': 'blackboard.html', 'document_root': str(frontend_root / 'Landing Page')}),
+    path('', serve, {'path': 'Landing Page/blackboard.html', 'document_root': str(frontend_root)}),
     path('admin/', admin.site.urls),
-    path('chat/', serve, {'path': 'chat.html', 'document_root': str(frontend_root / 'Chat' / 'pages')}),
-    path('styles/<path:path>', serve, {'document_root': str(frontend_root / 'Chat' / 'styles')}),
-    path('scripts/<path:path>', serve, {'document_root': str(frontend_root / 'Chat' / 'scripts')}),
-    path('image/<path:path>', serve, {'document_root': str(frontend_root / 'image')}),
-    path('<path:path>', serve, {'document_root': str(frontend_root / 'Chat' / 'pages')}),
-    path('frontend/<path:path>', serve, {'document_root': str(frontend_root)}),
+    re_path(r'^(?P<path>.+)$', serve, {'document_root': str(frontend_root)}),
 ]
 
 if settings.DEBUG:
